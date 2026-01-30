2a8a14f173590cb43efeee5732094b8309647953
KLFzero <1073332354@qq.com>
2026-01-30 23:46:36 +0900

    The purpose to use contribute fiction to make a character_mother version of script is not successed, the spefiction of different character need to copy from the mother version and change their paramater individually

--- ally.gd
+++ ally.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var speed: int = 200
 var health: int = 40
-var direction: Vector2 = Vector2(-1,0)
+var direction: Vector2 = Vector2(1,0)
 var attack_damage: int = 10
 @onready var attacktimer: Timer = $Attacktimer
 @onready var invincibilitytimer: Timer = $Invincibilitytimer
