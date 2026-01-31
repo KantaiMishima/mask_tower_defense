969e8f87ebd6e2b349ca0a06db82fb97373a71a9
KLFzero <1073332354@qq.com>
2026-01-31 13:47:12 +0900

    Mask drag finished!
    Now in construction of unit replacement based on bask application

--- masks/mask_ui.gd
+++ masks/mask_ui.gd
@@ -1,5 +1,6 @@
 extends Control
 
+# instance part========================================================================
 @export var mask_texture: TextureRect
 @export var mask_background: TextureRect
 @export var mask_name: Label
@@ -13,21 +14,3 @@ func init_mask(mask_data) -> void:
 
 	mask_texture.texture = mask_data.mask_texture
 	mask_name.text = mask_data.mask_name
-
-	#mouse_filter = MouseFilter.STOP  # For contruct any action
-
-#code below will be finish later to make the mask powerup function
-#
-#func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
-	#if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		
-		#if card_data and card_data.summon_scene:
-			#summon_ally(card_data.summon_scene)
-			
-			#queue_free()
-
-#
-#func summon_ally(scene: PackedScene) -> void:
-	#var ally = scene.instantiate() if Engine.get_version_info().major == 4 else scene.instance()
-	#get_tree().root.get_node("GameScene/AllySpawn").add_child(ally)
-	#ally.global_position = get_tree().root.get_node("GameScene/AllySpawn").global_position
---
+++ masks/mask_ui_action.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var is_dragging: bool = false
+var drag_offset: Vector2 = Vector2.ZERO
+var original_parent: Node = null  
+var original_position: Vector2 = Vector2.ZERO
+
+func _ready():
+	original_parent = get_parent()  # to the parent control node
+
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	print(event)
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			is_dragging = true
+			drag_offset = get_global_mouse_position() - global_position
+			original_position = global_position
+			z_index = 1000
+			get_tree().get_root().add_child(self)
+			print("dragging start")
+		else:
+			if is_dragging:
+				is_dragging = false
+				z_index = 1
+				original_parent.add_child(self)
+				global_position = original_position
+				print("dragging stop")
+
+func _process(_delta):
+	if is_dragging:
+		global_position = get_global_mouse_position() - drag_offset
