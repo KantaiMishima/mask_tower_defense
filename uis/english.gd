289af2d9b232e61ef4fae5fae20beb85e0a796af
KantaiMishima <mishimakantai@gmail.com>
2026-02-01 13:11:35 +0900

    feat: 操作説明画面を追加

---
+++ uis/english.gd
@@ -0,0 +1,13 @@
+extends BoxContainer
+
+@export var path = "res://demo/demo.tscn"
+# ゲームで一番使うボタンでスタートするようにする
+func _on_start_anywhere_clicked():
+	$AudioStreamPlayer.play()
+	# 登録した名前「SceneChanger」をそのまま使って関数を呼ぶ
+	SceneChanger.change_scene(path)
+
+func _input(event):
+	# 「マウスの左ボタンが押された瞬間」だけを判定
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		_on_start_anywhere_clicked()
